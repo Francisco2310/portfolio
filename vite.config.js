1a423df57224989aa92e9f3ebc2c0b13c8ba0a70
Francisco2310 <74802078+Francisco2310@users.noreply.github.com>
2026-02-01 15:53:19 -0300

    vite preload otimization

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  
+  build: {
+    modulePreload: {
+      polyfill: true
+    },
+    cssCodeSplit: false 
+  }
 })
