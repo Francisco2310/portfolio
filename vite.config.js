68c539713a7b5eb9c5353b4707556920f06e3144
Francisco2310 <74802078+Francisco2310@users.noreply.github.com>
2026-02-01 16:00:24 -0300

    pre load optimizations

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  build: {
-    modulePreload: {
-      polyfill: true
-    },
-    cssCodeSplit: false 
-  }
+  plugins: [vue(), tailwindcss(), viteSingleFile()],
+  
 })
